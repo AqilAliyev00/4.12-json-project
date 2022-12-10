d16fc7682b0951f02b8b64b21b70c8591576aa88
Vyrskal <28975125+Vyrskal@users.noreply.github.com>
2022-12-10 23:04:52 +0400

    Update students 3.py

--- students 3.py
+++ students 3.py
@@ -3,7 +3,7 @@ import json
 with open ('config.json','r',encoding="utf-8") as file:
     data = json.load(file)
  
-data["author"] ="Aqil"
+data["author"] ="test"
 data["server"]["port"] = 2024
 data["server"]["port2"] = 2025
 data["openInBrowser"] = True
